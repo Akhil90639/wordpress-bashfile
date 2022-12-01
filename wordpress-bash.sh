93148f2ed34538acf7670e0dbc63ee7f96b1ad96
root <root@ip-172-31-83-1.ec2.internal>
2022-12-01 11:30:00 +0000

    this is wordpress file

---
+++ wordpress-bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo yum -y install git 
+sudo yum -y install docker
+sudo usermod -a -G docker ec2-user
+sudo service docker start
+sudo chkconfig docker on
+sudo systemctl enable docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo git clone https://github.com/Akhil90639/wordpress.git
+cd wordpress
+sudo docker-compose up -d
