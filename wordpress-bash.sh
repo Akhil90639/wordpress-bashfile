ed3cfd2d4d304157db88164f11361230917640a2
Akhil <111115830+Akhil90639@users.noreply.github.com>
2023-02-11 11:38:46 +0530

    Update wordpress-bash.sh

--- wordpress-bash.sh
+++ wordpress-bash.sh
@@ -11,5 +11,5 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo git clone https://github.com/Akhil90639/wordpress.git
+sudo git clone https://github.com/Akhil90639/wordpress.git /var/www/html
 sudo docker-compose up -d
