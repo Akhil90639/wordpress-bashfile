a4a315646b6071b70f01a0abbdea59e2ce312b73
Akhil <111115830+Akhil90639@users.noreply.github.com>
2023-02-11 12:02:44 +0530

    Update wordpress-bash.sh

--- wordpress-bash.sh
+++ wordpress-bash.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 sudo yum -y install git 
 sudo yum -y install docker
-sudo yum -y install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
 sudo git clone https://github.com/Akhil90639/wordpress.git /var/www/html
 sudo usermod -a -G docker ec2-user
 sudo service docker start
@@ -12,4 +9,5 @@ sudo systemctl enable docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+cd wordpress
 sudo docker-compose up -d
